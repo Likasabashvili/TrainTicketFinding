75ba5f90686e667ae00dec6f4890a3507688a74a
Likasabashvili <sabashvili2009@gmail.com>
2026-04-18 22:10:09 +0400

    stations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('TrainTicketFinding');
---
+++ src/app/home/home.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Service } from '../service';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home implements OnInit {
+  stations: any[] = [];
+  constructor(private service: Service) {}
+
+  ngOnInit() {
+    this.fetchStations();
+  }
+
+  fetchStations() {
+    this.service.getStations().subscribe((data) => {
+      this.stations = data;
+    });
+  }
+}
